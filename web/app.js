b3808d0e0bd3b10f8c20bafa5f22548c75d16d8f
rubenqs12 <rubenqs12@users.noreply.github.com>
2026-08-29 18:04:05 +0200

    chore: align platform metadata for 1.2.10

--- web/app.js
+++ web/app.js
@@ -276,7 +276,7 @@
       headers: {
         'Content-Type': 'application/json',
         'Accept-Language': languageCode(),
-        'X-Emby-Authorization': 'MediaBrowser Client="Velora Web", Device="Browser", DeviceId="velora-web", Version="1.2.5", Language="' + languageCode() + '"'
+        'X-Emby-Authorization': 'MediaBrowser Client="Velora Web", Device="Browser", DeviceId="velora-web", Version="1.2.10", Language="' + languageCode() + '"'
       },
       body: JSON.stringify({ Username: username, Password: document.querySelector('#password').value })
     }).then(function (response) {
