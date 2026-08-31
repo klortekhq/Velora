90a658cf71655c17af6ed5c9601780e0486cf71d
rubenqs12 <rubenqs12@users.noreply.github.com>
2026-08-31 15:12:41 +0200

    release: prepare 1.2.42

--- web/app.js
+++ web/app.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
 
-  var APP_VERSION = '1.2.41';
+  var APP_VERSION = '1.2.42';
 
   var LANGUAGE_OPTIONS = [
     { value: 'auto', label: 'Automático', native: 'Automático' },
